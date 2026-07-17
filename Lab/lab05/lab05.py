3241947d9df29c7efe0184e84f1f3e4046c4f5e5
XLi-hub <1039131704@qq.com>
2026-07-17 20:59:43 +0800

    finish lab05

--- Lab/lab05/lab05.py
+++ Lab/lab05/lab05.py
@@ -27,7 +27,14 @@ def insert_items(s, before, after):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    i=0
+    while i<len(s):
+        if s[i]==before:
+            s.insert(i+1,after)
+            i+=2
+        else:
+            i+=1
+    return s 
 
 def group_by(s, fn):
     """Return a dictionary of lists that together contain the elements of s.
@@ -40,12 +47,12 @@ def group_by(s, fn):
     {9: [-3, 3], 4: [-2, 2], 1: [-1, 1], 0: [0]}
     """
     grouped = {}
-    for ____ in ____:
-        key = ____
+    for i in range(len(s)):
+        key = fn(s[i])
         if key in grouped:
-            ____
+            grouped[key].append(s[i])
         else:
-            grouped[key] = ____
+            grouped[key] = [s[i]]
     return grouped
 
 
@@ -71,7 +78,11 @@ def count_occurrences(t, n, x):
     2
     """
     "*** YOUR CODE HERE ***"
-
+    count=0
+    for i in range(n):
+        if next(t)==x:
+            count+=1
+    return count
 
 def repeated(t, k):
     """Return the first value in iterator t that appears k times in a row,
@@ -94,7 +105,16 @@ def repeated(t, k):
     """
     assert k > 1
     "*** YOUR CODE HERE ***"
-
+    count = 1
+    a=next(t)
+    while count < k:
+        curr = next(t)
+        if curr == a:
+            count += 1
+        else:
+            count = 1
+            a = curr 
+    return a  
 
 def sprout_leaves(t, leaves):
     """Sprout new leaves containing the labels in leaves at each leaf of
@@ -129,7 +149,9 @@ def sprout_leaves(t, leaves):
           1
           2
     """
-    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return tree(label(t), [tree(leaf) for leaf in leaves])
+    return tree(label(t), [sprout_leaves(b, leaves) for b in branches(t)])
 
 
 def partial_reverse(s, start):
@@ -145,6 +167,7 @@ def partial_reverse(s, start):
     [1, 2, 7, 6, 5, 3, 4]
     """
     "*** YOUR CODE HERE ***"
+    s[start:] = s[start:][::-1]
 
 
 
