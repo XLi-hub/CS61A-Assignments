c0c5bd98c356026aa7320f9d43fe4c055c53f1a8
XLi-hub <1039131704@qq.com>
2026-07-17 18:12:26 +0800

    finish hw04

--- HW/hw04/hw04.py
+++ HW/hw04/hw04.py
@@ -13,6 +13,11 @@ def shuffle(s):
     """
     assert len(s) % 2 == 0, 'len(seq) must be even'
     "*** YOUR CODE HERE ***"
+    list=[]
+    for i in range(len(s)//2):
+        list.append(s[i])
+        list.append(s[i+len(s)//2])
+    return list
 
 
 def deep_map(f, s):
@@ -38,6 +43,11 @@ def deep_map(f, s):
     True
     """
     "*** YOUR CODE HERE ***"
+    for i in range(len(s)):
+        if type(s[i])==list:
+            deep_map(f,s[i])
+        else:
+            s[i]=f(s[i])
 
 
 HW_SOURCE_FILE=__file__
@@ -47,11 +57,12 @@ def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
     "*** YOUR CODE HERE ***"
-
+    return ['planet', mass]
 def mass(p):
     """Select the mass of a planet."""
     assert is_planet(p), 'must call mass on a planet'
     "*** YOUR CODE HERE ***"
+    return p[1]
 
 def is_planet(p):
     """Whether p is a planet."""
@@ -104,7 +115,11 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if is_planet(m):
+        return True
+    left_torque = length(left(m))*total_mass(end(left(m)))
+    right_torque = length(right(m))*total_mass(end(right(m)))
+    return left_torque==right_torque and balanced(end(left(m))) and balanced(end(right(m)))
 
 def berry_finder(t):
     """Returns True if t contains a node with the value 'berry' and 
@@ -124,7 +139,12 @@ def berry_finder(t):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if label(t)=='berry':
+        return True
+    for b in branches(t):
+        if berry_finder(b):
+            return True
+    return False
 
 HW_SOURCE_FILE=__file__
 
@@ -139,8 +159,9 @@ def max_path_sum(t):
     17
     """
     "*** YOUR CODE HERE ***"
-
-
+    if is_leaf(t):
+        return label(t)
+    return label(t) + max([max_path_sum(b) for b in branches(t)])
 def mobile(left, right):
     """Construct a mobile from a left arm and a right arm."""
     assert is_arm(left), "left must be an arm"
